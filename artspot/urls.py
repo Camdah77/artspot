b6cb975f76a168406ba6fc44517eaa4d37eb5c65
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-10 19:20:15 +0000

    in url.py added path('', include('artistspot.urls'), name='artistspot-urls')

--- artspot/urls.py
+++ artspot/urls.py
@@ -1,7 +1,9 @@
 #from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
 #    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('artistspot.urls'), name='artistspot-urls'),
 ]

259df9573e97436b931b2378f1227385bcfbf0d0
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-11 06:12:49 +0000

    Issues: LookupError: No installed app with label 'admin'.

--- artspot/urls.py
+++ artspot/urls.py
@@ -1,9 +1,6 @@
-#from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-#    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path('', include('artistspot.urls'), name='artistspot-urls'),
+    path('admin/', admin.site.urls),
 ]

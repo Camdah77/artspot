5332c28e1b435594f6c307dd60d2cf4712989bb5
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-11 05:55:50 +0000

    Readme: Added the structure and headings

--- artistspot/settings.py
+++ artistspot/settings.py
@@ -40,9 +40,6 @@ application = get_wsgi_application()
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -69,7 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
      'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+     'django.contrib.sessions',
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
